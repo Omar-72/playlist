a3059b7d20b436bca1225794870a9cbccca3601a
Glitch (omar-playlist) <none>
2022-03-09 23:22:39 +0000

    🎊📩 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 // Complete Day 1 goals here
-let songs=["Blinding lights", "Take on me", "Smooth criminal", "Industry baby"]
-let aritst=["The Weekend", "Ah-a", "Michael Jackson", "Lil nas X"]
-let image=["https://storage.briwebapp.net/2020/06/7b93b245-wd.jpg", "https://cdns-images.dzcdn.net/images/cover/e0ce8977ab98d73bcea00fc838ece034/500x500.jpg", "https://www.lololyrics.com/img/cover/free/18383.jpeg", "https://i1.sndcdn.com/artworks-j1zGqEwf1STpMyZK-PRpXPg-t500x500.jpg"]
-let link=["https://www.youtube.com/watch?v=fHI8X4OXluQ", "https://www.youtube.com/watch?v=djV11Xbc914", "https://www.youtube.com/watch?v=h_D3VFfhvs4", "https://www.youtube.com/watch?v=HCq1OcAEAm0"]
-let name=[".title",".artist", ".image",".link"]
-let put=[".songs", ".artists", ".images", ".links"]
+let songs=[{track:"Blinding lights"}, 
+          {track:"Take on me"}, 
+           {track:"Smooth criminal"}, 
+           {track:"Industry baby"}];
+let aritst=[{singer:"The Weekend"}, 
+            {singer:"Ah-a"}, 
+            {singer:"Michael Jackson"}, 
+            {singer:"Lil nas X"}];
+/*let image=[<img src="https://storage.briwebapp.net/2020/06/7b93b245-wd.jpg"/>, 
+        <img src="https://cdns-images.dzcdn.net/images/cover/e0ce8977ab98d73bcea00fc838ece034/500x500.jpg"/>, 
+          <img src="https://www.lololyrics.com/img/cover/free/18383.jpeg"/>, 
+          <img src="https://i1.sndcdn.com/artworks-j1zGqEwf1STpMyZK-PRpXPg-t500x500.jpg"/>]*/
+let link=[{address:"https://www.youtube.com/watch?v=fHI8X4OXluQ"}, 
+          {address:"https://www.youtube.com/watch?v=djV11Xbc914"}, 
+          {address:"https://www.youtube.com/watch?v=h_D3VFfhvs4"}, 
+           {adrees:"https://www.youtube.com/watch?v=HCq1OcAEAm0"}];
+let name=[".title",".artist", ".image",".link"];
+let put=[".songs", ".artists", ".images", ".links"];
 $(".add").click(function(){
   let music=$(name[0]).val();
   $(put[0]).append(music);
@@ -21,10 +33,17 @@ $(".add").click(function(){
 function displaySongInfo() {
   // Complete the Day 2 goals inside this function
   songs.forEach(function(song){
-    $(".songs").append(`<li>${song}</li>`);
-    
-  });
+    $(".songs").append(`<li>${song.track}</li>`);
+});
+    aritst.forEach(function(sing){
+    $(".artists").append(`<li>${sing.singer}</li>`); 
+});
+
+  link.forEach(function(ip){
+    $(".links").append(`<li>${ip.address}</li>`); 
+});
 }
+  
 
 function emptySongInfo() {
   /* this function empties the divs each time the button is 
