38931590c80ed24e3a111b3258bd6dc5efbe5d2d
Glitch (omar-playlist) <none>
2022-03-16 21:32:33 +0000

    🥫🔰 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -7,16 +7,25 @@ let aritst=[{singer:"The Weekend"},
             {singer:"Ah-a"}, 
             {singer:"Michael Jackson"}, 
             {singer:"Lil nas X"}];
-/*let image=[<img src="https://storage.briwebapp.net/2020/06/7b93b245-wd.jpg"/>, 
-        <img src="https://cdns-images.dzcdn.net/images/cover/e0ce8977ab98d73bcea00fc838ece034/500x500.jpg"/>, 
-          <img src="https://www.lololyrics.com/img/cover/free/18383.jpeg"/>, 
-          <img src="https://i1.sndcdn.com/artworks-j1zGqEwf1STpMyZK-PRpXPg-t500x500.jpg"/>]*/
+let image=[{picture:"https://storage.briwebapp.net/2020/06/7b93b245-wd.jpg"}, 
+           {picture:"https://cdns-images.dzcdn.net/images/cover/e0ce8977ab98d73bcea00fc838ece034/500x500.jpg"}, 
+         {picture:"https://www.lololyrics.com/img/cover/free/18383.jpeg"}, 
+           {picture:"https://i1.sndcdn.com/artworks-j1zGqEwf1STpMyZK-PRpXPg-t500x500.jpg"}];
 let link=[{address:"https://www.youtube.com/watch?v=fHI8X4OXluQ"}, 
           {address:"https://www.youtube.com/watch?v=djV11Xbc914"}, 
           {address:"https://www.youtube.com/watch?v=h_D3VFfhvs4"}, 
            {adrees:"https://www.youtube.com/watch?v=HCq1OcAEAm0"}];
 let name=[".title",".artist", ".image",".link"];
 let put=[".songs", ".artists", ".images", ".links"];
+let tracks=[
+  {
+    title:"Blinding lights",
+    artist:"The Weekend",
+    image:"https://storage.briwebapp.net/2020/06/7b93b245-wd.jpg",
+    link:"https://www.youtube.com/watch?v=fHI8X4OXluQ"
+  }
+
+];
 $(".add").click(function(){
   let music=$(name[0]).val();
   $(put[0]).append(music);
@@ -32,16 +41,27 @@ $(".add").click(function(){
 });
 function displaySongInfo() {
   // Complete the Day 2 goals inside this function
-  songs.forEach(function(song){
+  /*songs.forEach(function(song){
     $(".songs").append(`<li>${song.track}</li>`);
 });
     aritst.forEach(function(sing){
     $(".artists").append(`<li>${sing.singer}</li>`); 
 });
 
-  link.forEach(function(ip){
-    $(".links").append(`<li>${ip.address}</li>`); 
+  image.forEach(function(pic){
+    $(".images").append(`<li><img src=${pic.picture}/></li>`); 
 });
+  
+  link.forEach(function(ip){
+    $(".links").append(`<li><a href="${ip.address}">Listen now!!</a></li>`); 
+});*/
+  
+  tracks.forEach(function(track){
+    $(".rows").append(`<tr>
+      <td><img src=${track.image}</td>
+    </tr>`)
+    
+  });
 }
   
 
